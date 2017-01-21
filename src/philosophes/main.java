74e63a2852c661090fa32791b3bc2c90331e6b3d
Léo Pons <leo.pons@wanadoo.fr>
2017-01-21 16:09:27 +0100

    Suppr action lacher fourchettes

--- src/philosophes/main.java
+++ src/philosophes/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Table tab = new Table(10, 5, 5, -2, 1); 	// effectif, seuilFaim, deltaPenser, deltaManger, deltaFamine
+		Table tab = new Table(6, 5, 5, -2, 1); 	// effectif, seuilFaim, deltaPenser, deltaManger, deltaFamine
 		tab.run();
 
 	}
