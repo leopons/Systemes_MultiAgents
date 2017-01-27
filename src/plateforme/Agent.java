1bfee1c218a92d5bf031a7a9465eacdcfa95e7cb
Léo Pons <leo.pons@wanadoo.fr>
2017-01-27 09:35:05 +0100

    Commentaires Agent

--- src/plateforme/Agent.java
+++ src/plateforme/Agent.java
@@ -2,13 +2,24 @@ package plateforme;
 
 import java.util.ArrayList;
 
-public class Agent {
+public abstract class Agent {
+	
+	// Un agent est un des objets agissants de notre système.
+	// Il est lié à un environnement et à une liste d'actions qu'il peut effectuer.
 	
 	protected Environnement env;
 	protected ArrayList<Action> actions;
 
-	public void percevoir(){
+	// A chaque fois qu'un agent est appelé, c'est au travers de sa boucle procédurale.
+	// Il va donc agir, sur la base de sa délibération. (La partie percevoir est incluse
+	// dans la fonction de délibération, au niveai de la vérification des conditions).
+	
+	public void boucle_procedurale(){
+		this.agir(this.deliberer());
 	}
+	
+	// Dans deliberer(), l'agent parcourt la liste des actions qu'il peut effectuer, vérifie les conditions
+	// de ces actions en observant l'environnement, et choisit celle qu'il peut effectuer.
 	public Action deliberer(){
 		for (Action a : actions){
 			if (a.check_conditions(this)){
@@ -17,15 +28,10 @@ public class Agent {
 		}
 		return null;
 	}
+	
+	// Dans agir(), l'agent effectue une action (qui aura été choisie par deliberer())
 	public void agir(Action a){
 		a.effectuer(this);
 	}
-	
-	public void boucle_procedurale(){
-		this.percevoir();
-		this.agir(this.deliberer());
-	}
-	
-	
 	
 }
