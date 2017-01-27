e7f804921edfde271d4e7c4555254f995dd4e196
Léo Pons <leo.pons@wanadoo.fr>
2017-01-27 09:15:56 +0100

    Commentaires Environnement

--- src/plateforme/Environnement.java
+++ src/plateforme/Environnement.java
@@ -2,8 +2,14 @@ package plateforme;
 
 import java.util.ArrayList;
 
-public class Environnement {
+public abstract class Environnement {
 
+	// L'environnement est l'objet contenant notre système multi-agents.
+	// La classe environnement sera donc instanciée une unique fois, puis
+	// on utilisera la méthode run pour lancer le système.
+	
+	// L'objet regroupe les différents agents, et les données propres à l'environnement.
+	
 	protected ArrayList<Agent> agents;
 	protected ArrayList<Donnee> donnees;
 	
@@ -11,7 +17,11 @@ public class Environnement {
 		return donnees;
 	}
 	
-	public void bilan(){}
+	// On choisira un mode de fonctionnement synchrone pour nos systèmes.
+	// Les différents agents effectuent donc leurs boucles procédurales tour à tour.
+	// La fonction bilan est appelée après chaque tour des agents. Elle est là pour
+	// afficher si besoin certaines valeurs représentatives du système.
+	// (Il faudra l'implémenter dans le modèle, en fonction des données à afficher)
 	
 	public void run(){
 		while (true) {
@@ -21,5 +31,7 @@ public class Environnement {
 				this.bilan();
 		}
 	}
+	
+	public abstract void bilan();
 	
 }
