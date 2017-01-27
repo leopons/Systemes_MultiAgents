c6b9733428ebd631be8c00cfe77bddbf478ee65f
Léo Pons <leo.pons@wanadoo.fr>
2017-01-27 09:37:15 +0100

    Commentaires Action

--- src/plateforme/Action.java
+++ src/plateforme/Action.java
@@ -2,6 +2,10 @@ package plateforme;
 
 public abstract class Action {
 	
+	// Une action est constituée de deux méthodes : une qui vérifie ses conditions d'éxecution,
+	// Et une autre qui effectue l'action proprement dit.
+	// Le détail de ces méthodes est implémenté selon le système et selon l'action.
+	
 	public abstract boolean check_conditions(Agent agent);
 	public abstract void effectuer(Agent agent);
 
